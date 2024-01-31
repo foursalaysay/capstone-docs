f22d146a71e43eebd501c20b3260aee97014331f
John Kyle Salaysay <foursalaysay@gmail.com>
2024-01-31 23:13:40 +0800

    homepage documentation capstone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+'use client'
+
 import Image from "next/image";
+
+import hacker from '/public/hacker.svg'
+
+// bg-gradient-to-l from-black to-blue-900
 
 export default function Home() {
   return (
-      <div>
-        
+      <div className="w-50% h-screen bg-gradient-to-l from-black to-blue-900">
+        <div className="flex flex-col items-center justify-center">
+           <Image
+           className=""
+           src={hacker}
+           height={500}
+           width={500}
+           objectFit="cover"
+           objectPosition="center"
+           alt="background"
+           />
+           <h1 className="text-[150px] font-black text-white absolute ">Mind<span className="text-blue-500">Scapetor</span></h1>
+        </div>
       </div>
   );
 }
